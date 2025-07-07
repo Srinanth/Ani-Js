9a09e60959e602f3c3d6337b1f88510913137fcd
Srinanth <srinanthmv845@gmail.com>
2025-07-07 18:58:12 +0530

    analytics

--- Web/Ani-Js/src/App.jsx
+++ Web/Ani-Js/src/App.jsx
@@ -3,16 +3,20 @@ import UsagePage from './Pages/Usage';
 import PlayGroundPage from './Pages/PlayGround';
 import AnimationsPage from './Pages/Animations';
 import { Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 
 
 function App() {
   return (
+    <>
+    <Analytics />
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/usage" element={<UsagePage />} />
       <Route path="/playground" element={<PlayGroundPage />} />
       <Route path="/animations" element={<AnimationsPage />} />
     </Routes>
+    </>
   );
 
 }
